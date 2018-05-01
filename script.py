075e56cbf1d7728392a3f5f759f5abf3a25c43bc
Duncan Leo <duncanleo97@gmail.com>
2018-05-01 16:56:30 +0800

    update script to use dotenv and google api client python

--- script.py
+++ script.py
@@ -1,53 +1,60 @@
 from dotenv import load_dotenv
-from configparser import RawConfigParser
 from random import randint
 from telegram.ext import Updater, CommandHandler
-from google.cloud import translate
+from googleapiclient.discovery import build
 from google_images_download import google_images_download
+from os import getenv
 
-load_dotenv()
+load_dotenv(dotenv_path='.env')
+
+service = build('translate', 'v2', developerKey=getenv('GOOGLE_API_KEY'))
+
 
 def start(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Send me some weird characters.")
+  bot.sendMessage(chat_id=update.message.chat_id,
+                  text="Send me some weird characters.")
+
 
 def translateOnly(bot, update):
-    try:
-        client = translate.Client.from_service_account_json("C:\Python27\PicTranslate-0f848a4ac055.json")
-        text = update.message["text"].split(' ', 1)
-        result = client.translate(text[1])
-    except Exception as e:
-        print(e)
+  try:
+    text = update.message["text"].split(' ', 1)
+    response = service.translations().list(
+      target='en',
+      q=' '.join(text[1:])
+    ).execute()
+  except Exception as e:
+    print(e)
 
-    bot.sendMessage(chat_id=update.message.chat_id, text=result["translatedText"])
+  bot.sendMessage(chat_id=update.message.chat_id,
+                  text=response['translations'][0]['translatedText'])
 
 
 def pictranslate(bot, update):
-    try:
-        client = translate.Client.from_service_account_json("C:\Python27\PicTranslate-0f848a4ac055.json")
-        text = update.message["text"].split(' ', 1)
-        result = client.translate(text[1])
-    except Exception as e:
-        print(e)
+  try:
+    text = update.message["text"].split(' ', 1)
+    response = service.translations().list(
+      target='en',
+      q=' '.join(text[1:])
+    ).execute()
+  except Exception as e:
+    print(e)
 
-    try:
-        imageClient = google_images_download.googleimagesdownload()
-        imageURL = imageClient.download(
-            {
-                "keywords": result["translatedText"],
-                "limit": randint(1,21),
-                "print_urls": True
-            })
-    except Exception as e:
-        print(e)
+  try:
+    imageClient = google_images_download.googleimagesdownload()
+    imageURL = imageClient.download(
+      {
+        "keywords": result["translatedText"],
+        "limit": randint(1, 21),
+        "print_urls": True
+      })
+  except Exception as e:
+    print(e)
 
-    bot.sendPhoto(chat_id=update.message.chat_id, photo=imageURL, caption=result["translatedText"])
+  bot.sendPhoto(chat_id=update.message.chat_id, photo=imageURL,
+                caption=response['translations'][0]['translatedText'])
 
-def getToken():
-    config = RawConfigParser()
-    config.read("config.ini")
-    return config.get('Token', 'telegram')
 
-updater = Updater(token=getToken())
+updater = Updater(token=getenv('TELEGRAM_BOT_TOKEN'))
 start_handler = CommandHandler('start', start)
 translate_handler = CommandHandler('translate', translateOnly)
 pictranslate_handler = CommandHandler('pictranslate', pictranslate)
